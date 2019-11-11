8dee60bebc28a9a43b39e19c04b58a989ddff3ae
Yossie Ruben Advindo <51661790+yossieadvindo@users.noreply.github.com>
2019-11-11 19:56:16 +0700

    1

--- app/src/main/java/id/ac/ukdw/timbangantri/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/timbangantri/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,23 +19,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        btnLogin.setOnClickListener({login(emailLogin.text.toString(), passwordLogin.text.toString())})
+        btnLogin.setOnClickListener {
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(emailLogin.text.toString(), passwordLogin.text.toString())
+                .addOnSuccessListener  {
+                    Toast.makeText(baseContext, "berhasil login", Toast.LENGTH_LONG).show()
+                    val i: Intent = Intent(baseContext, Home::class.java)
+                    startActivity(i)
+                }.addOnFailureListener {
+                    //FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString())
+                    Toast.makeText(baseContext, "berhasil gagal", Toast.LENGTH_LONG).show()
+                }
+
+        }
+
         txtsignup.setOnClickListener({showRegisterActivity()})
 
     }
-    fun login(email: String, password:String){
-        if (email.equals("coba@gmail.com") && password.equals("1234")){
-            showHomeActivity()
-        }else{
-            Toast.makeText(this, "Login Gagal", Toast.LENGTH_LONG).show()
-        }
-    }
 
-    fun showHomeActivity(){
-        var i: Intent = Intent(this, Home::class.java)
-//        i.putExtra()
-        startActivity(i)
-    }
 
     fun showRegisterActivity(){
         var i: Intent = Intent(this, Register::class.java)
--- app/src/main/java/id/ac/ukdw/timbangantri/Register.kt
+++ app/src/main/java/id/ac/ukdw/timbangantri/Register.kt
@@ -1,13 +1,27 @@
 package id.ac.ukdw.timbangantri
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login.*
 
 class Register : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sing_up)
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(emailLogin.text.toString(), passwordLogin.text.toString())
+            .addOnSuccessListener  {
+                Toast.makeText(baseContext, "berhasil login", Toast.LENGTH_LONG).show()
+                val i: Intent = Intent(baseContext, Home::class.java)
+                startActivity(i)
+            }.addOnFailureListener {
+                //FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString())
+                Toast.makeText(baseContext, "berhasil gagal", Toast.LENGTH_LONG).show()
+            }
     }
 
 }

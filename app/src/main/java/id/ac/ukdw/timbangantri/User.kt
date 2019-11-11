13e1230f5922a817e0044c4cc49816fd532a193e
Yossie Ruben Advindo <51661790+yossieadvindo@users.noreply.github.com>
2019-11-11 21:05:57 +0700

    login

--- app/src/main/java/id/ac/ukdw/timbangantri/User.kt
+++ app/src/main/java/id/ac/ukdw/timbangantri/User.kt
@@ -1,3 +1,3 @@
 package id.ac.ukdw.timbangantri
 
-data class User(var Email: String, var Password: String, var nama_dpn: String, var nama_blkng: String, var no_tlp: Integer)
+data class User(var Email: String, var Password: String, var nama_dpn: String, var nama_blkng: String, var no_tlp: String)

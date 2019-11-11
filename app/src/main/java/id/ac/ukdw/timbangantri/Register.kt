83f40e8bdc965e6166ef94e8c0bb63803d3ff972
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-11-11 21:45:08 +0700

    okeoke

--- app/src/main/java/id/ac/ukdw/timbangantri/Register.kt
+++ app/src/main/java/id/ac/ukdw/timbangantri/Register.kt
@@ -2,26 +2,56 @@ package id.ac.ukdw.timbangantri
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.sing_up.*
 
 class Register : AppCompatActivity(){
+    lateinit var db:DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.sing_up)
-        FirebaseAuth.getInstance()
-            .signInWithEmailAndPassword(emailLogin.text.toString(), passwordLogin.text.toString())
-            .addOnSuccessListener  {
-                Toast.makeText(baseContext, "berhasil login", Toast.LENGTH_LONG).show()
-                val i: Intent = Intent(baseContext, Home::class.java)
-                startActivity(i)
-            }.addOnFailureListener {
-                //FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString())
-                Toast.makeText(baseContext, "berhasil gagal", Toast.LENGTH_LONG).show()
+
+        db = FirebaseDatabase.getInstance().getReference("user")
+
+        loginkuy.setOnClickListener {
+            val i: Intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(i)
+        }
+
+        btnDaftar.setOnClickListener {
+            if(!edtEmail.text.toString().equals("") && !edtPass.text.toString().equals("") && !txtNamaDepan.text.toString().equals("") &&
+                    !txtNamaBlkng.text.toString().equals("") && !txtNoTlp.text.toString().equals("")){
+                isiData()
+                Toast.makeText(baseContext, "TERDAFTAR", Toast.LENGTH_LONG).show()
             }
+//            else if(edtPass.text.toString().equals(edtPassAgain.text.toString())) {
+//                Toast.makeText(baseContext, "Password Gak Konsisten", Toast.LENGTH_LONG).show()
+//            }
+            else{
+                Toast.makeText(baseContext, "EMAIL ATAU PASSWORD KOSONG", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
+    fun isiData(){
+        var email: TextView = findViewById(R.id.edtEmail)
+        var pass: TextView = findViewById(R.id.edtPass)
+        var nama_dep: TextView = findViewById(R.id.txtNamaDepan)
+        var nama_belk: TextView = findViewById(R.id.txtNamaBlkng)
+        var no_telp: TextView = findViewById(R.id.txtNoTlp)
+
+        var user = User(email.text.toString(), pass.text.toString(), nama_dep.text.toString(), nama_belk.text.toString(), no_telp.text.toString())
+        db.child(nama_dep.text.toString()).setValue(user)
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.text.toString(), pass.text.toString())
+    }
 }
+

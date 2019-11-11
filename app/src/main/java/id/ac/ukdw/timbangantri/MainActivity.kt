4ccf4185df4901155f3e5abe391f5c23410f5648
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-11-11 19:57:12 +0700

    okeoke

--- app/src/main/java/id/ac/ukdw/timbangantri/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/timbangantri/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,28 +19,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        btnLogin.setOnClickListener({login(emailLogin.text.toString(), passwordLogin.text.toString())})
-        txtsignup.setOnClickListener({showRegisterActivity()})
+//        btnLogin.setOnClickListener({login(emailLogin.text.toString(), passwordLogin.text.toString())})
+//        txtsignup.setOnClickListener({showRegisterActivity()})
 
-    }
-    fun login(email: String, password:String){
-        if (email.equals("coba@gmail.com") && password.equals("1234")){
-            showHomeActivity()
-        }else{
-            Toast.makeText(this, "Login Gagal", Toast.LENGTH_LONG).show()
+        btnLogin.setOnClickListener {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                  emailLogin.text.toString(), passwordLogin.text.toString())
+                    .addOnCompleteListener {
+                        Toast.makeText(baseContext, "Login Berhasil", Toast.LENGTH_LONG).show()
+                        val i:Intent = Intent(baseContext, Home::class.java)
+                        startActivity(i)
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(baseContext, "Login Gagal", Toast.LENGTH_LONG).show()
+                    }
         }
+
+        txtsignup.setOnClickListener{
+            val i:Intent = Intent(baseContext, Register::class.java)
+            startActivity(i)
+        }
+
     }
 
-    fun showHomeActivity(){
-        var i: Intent = Intent(this, Home::class.java)
-//        i.putExtra()
-        startActivity(i)
-    }
-
-    fun showRegisterActivity(){
-        var i: Intent = Intent(this, Register::class.java)
-//        i.putExtra()
-        startActivity(i)
-    }
+//    fun login(email: String, password:String){
+//        if (email.equals("coba@gmail.com") && password.equals("1234")){
+//            showHomeActivity()
+//        }else{
+//            Toast.makeText(this, "Login Gagal", Toast.LENGTH_LONG).show()
+//        }
+//    }
+//
+//    fun showHomeActivity(){
+//        var i: Intent = Intent(this, Home::class.java)
+////        i.putExtra()
+//        startActivity(i)
+//    }
+//
+//    fun showRegisterActivity(){
+//        var i: Intent = Intent(this, Register::class.java)
+////        i.putExtra()
+//        startActivity(i)
+//    }
 
 }
